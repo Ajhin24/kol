58493231e4deb756c83fe8b74cb344293590ea14
Ajhin24 <108016079+Ajhin24@users.noreply.github.com>
2022-06-23 00:47:18 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Nav bar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+         <img src="GamingHub.jpeg"style="width: 10%"height="80">
+        <label class="logo">Gaming Hub</label>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Feedback</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
